1589d5041ba4a74730fe59682504bdbccbea9154
tetolivares <tetolivares@gmail.com>
2020-12-12 17:55:10 -0400

    first commit

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Address;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AddressFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Address::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->word(random_int(2, 8)),
+            'name' => $this->faker->word(),
+            'detail' => $this->faker->address(random_int(160, 480)),
+        ];
+    }
+}
