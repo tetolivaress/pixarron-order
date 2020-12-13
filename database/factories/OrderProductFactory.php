07b4805d9a4fd2e0511936815d94e67534d2f5a1
tetolivares <tetolivares@gmail.com>
2020-12-13 15:48:07 -0400

    seeders working with belongsToMany

--- database/factories/OrderProductFactory.php
+++ database/factories/OrderProductFactory.php
@@ -7,22 +7,11 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OrderProductFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = OrderProduct::class;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
-            //
+            'order_id' => random_int(1, 5),
+            'address_id' => random_int(1, 5)
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Database\Seeders\UserTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-					UserTableSeeder::class
-				]);
+            UserTableSeeder::class,
+            OrderSeeder::class,
+            ]);
 		}
 }
