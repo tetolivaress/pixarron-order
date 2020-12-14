fe4eb3b7604e96c214ed0831cc76bc5da91912e8
tetolivares <tetolivares@gmail.com>
2020-12-14 14:06:40 -0400

    more removed comments

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -7,18 +7,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class AddressFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Address::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
